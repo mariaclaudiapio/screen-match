708f2307125422d0cf43f4449763970aa33e8d74
Maria Cláudia <mariaclaudiapio@gmail.com>
2026-01-05 16:45:38 -0300

    criacao de pacotes e de setters

--- Principal.java
+++ Principal.java
@@ -1,19 +1,20 @@
+import br.com.alura.screematch.modelos.Filme;
+
 public class Principal
 {
     public static void main(String[] args)
     {
-        // Criação do objeto 'Meu Filme'
+        // Criação do objeto 'Meu br.com.alura.screematch.modelos.Filme'
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Harry Potter e a Pedra Filosofal";
-        meuFilme.anoDeLancamento = 2001;
-        meuFilme.duracaoEmMinutos = 155;
+        meuFilme.setNome("Harry Potter e a Pedra Filosofal");
+        meuFilme.setAnoDeLancamento(2001);
+        meuFilme.setDuracaoEmMinutos(155);
 
         // Invocação do método
         meuFilme.exibeFichaTecnica();
 
         meuFilme.avalia(8);
         meuFilme.avalia(5);
-        meuFilme.avalia(10);
         meuFilme.avalia(10);
 
         System.out.println("O total de avaliações é: " + meuFilme.getTotalDeAvaliacoes() + ".");
